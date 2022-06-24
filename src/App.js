3258c9ab8b8c24efd7c786e72e4f64f67107a3c6
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-06-23 22:01:27 -0700

    Added the base folders and style files.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <> 
+      <Router>
+        <Route path="/" exact component={HomePage}/>
+      </Router>
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+            
+            <header>
+                <img alt="logo" />
+                <nav>
+
+                </nav>
+            </header>
+            )
+}
